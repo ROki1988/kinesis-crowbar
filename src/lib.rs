8f87e033e46a25396d63124b54f3c2f616f282c8
ROki1988 <roki.swindler@gmail.com>
2017-12-06 01:47:55 +0900

    decode data

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,19 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
+extern crate data_encoding;
+
+use data_encoding::BASE64;
+
+
 lambda!(|event, context| {
 
     let xs: Vec<Record> = serde_json::from_value(event["Records"].clone())?;
-    Ok(xs)
+    let h = xs.into_iter()
+        .filter_map(|x| BASE64.decode(x.kinesis.data.as_bytes()).ok())
+        .filter_map(|x| String::from_utf8(x).ok())
+        .collect::<Vec<String>>();
+    Ok(h)
 });
 
 #[derive(Serialize, Deserialize, Debug)]
